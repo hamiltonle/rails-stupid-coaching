6df8e160ea1d0da8efa1362b81ea46a1b766b5a7
Hamilton Le <hamilton.m.le@gmail.com>
2017-06-12 12:27:08 +0800

    solved

---
+++ app/controllers/coaching_controller.rb
@@ -0,0 +1,17 @@
+class CoachingController < ApplicationController
+  def answer
+    @question = params[:question]
+
+    if @question == "i am going to work right now!"
+      @answer = "Good!"
+    elsif @question.include? "?"
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+
+
+  def ask
+  end
+end
